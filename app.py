550ad8934e8befe599b57db604004f6b06c0b368
GemmaMorales <gemmaamorales@gmail.com>
2020-05-14 03:51:22 +0000

    saved first commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 
 from twilio.rest import Client
 import requests
-account_sid = 'AC6607710a8da5a23b7fa63232c27efd86'
-auth_token = 'a20d91042265b4e138517e278362614c'
+account_sid = 'xxxxxxxxxxx'
+auth_token = 'xxxxxxxxxxx'
 client = Client(account_sid, auth_token)
 r = requests.get('http://api.open-notify.org/astros.json')
 people = r.json()
@@ -10,7 +10,7 @@ number_iss = people['number']
 Message = 'Hi Fun fact,Number of people in space right now is '+str(number_iss)
 #formulate the message that will be sent
 message = client.messages.create(
-    to="+13124201017",
-    from_="+12058755998",
+    to="+1xxxxxxxxxx",
+    from_="+1xxxxxxxxxx",
     body=Message)
 print(message.sid)
